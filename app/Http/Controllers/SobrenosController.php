8ec483271d497e19ff9c88e34b69b96589ac389c
newtoncutrim <newtoncutrim777@gmail.com>
2023-07-31 15:11:33 -0300

    template

--- app/Http/Controllers/SobrenosController.php
+++ app/Http/Controllers/SobrenosController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class SobrenosController extends Controller
 {
-    public function sobrenos(): string {
-        return view('site/sobre-nos');
+    public function sobrenos() {
+        return view('site.sobre-nos');
     }
 }
