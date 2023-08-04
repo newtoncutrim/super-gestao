e7225960466a4f7d41f8179db0acd82693dfba6f
newtoncutrim <newtoncutrim777@gmail.com>
2023-08-03 21:36:44 -0300

    add campo table fornecedor

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SiteContato;
 use Faker\Core\Number;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato(Request $r) {
+    public function contato(Request $r, SiteContato $contato) {
         
         return view('site.contato');
     }
